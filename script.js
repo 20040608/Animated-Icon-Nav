edc33fbb124e65b2cf843a0b7f154f6d9f685fa9
20040608 <issakvell@gmail.com>
2023-10-24 15:05:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+window.addEventListener("DOMContentLoaded",() => {
+	const app = new App(".app");
+});
+
+class App {
+	constructor(qs) {
+		this.el = document.querySelector(qs);
+		this.page = 1;
+
+		this.el?.addEventListener("click",this.viewPage.bind(this));
+	}
+	setPage(number) {
+		const nav = document.querySelector("[data-nav]");
+
+		nav?.classList.remove(`nav--tilt${this.page}`);
+		this.page = number;
+		nav?.classList.add(`nav--tilt${this.page}`);
+	}
+	viewPage(e) {
+		e.preventDefault();
+		let parent = e.target;
+
+		while (parent && !parent.hasAttribute("data-nav-item"))
+			parent = parent.parentElement;
+
+		if (parent) {
+			
+			const pageNumber = +parent.getAttribute("data-nav-item");
+
+			if (pageNumber !== this.page) {
+				this.setPage(pageNumber);
+
+				const items = document.querySelectorAll("[data-nav-item]");
+
+				Array.from(items).forEach(item => {
+					item.removeAttribute("aria-describedby");
+				});	
+
+				parent.setAttribute("aria-describedby","current");
+
+				this.spawnCards();
+			}
+		}
+	}
+	spawnCards() {
+		const flyOut = "card--fly-out";
+		const cards = document.querySelectorAll("[data-card]");
+
+		Array.from(cards).forEach(card => {
+			card.classList.remove(flyOut);
+			void card.offsetWidth;
+			card.classList.add(flyOut);
+		});
+	}
+}
